0f9baa4d0ad7c7d1acd45bfea42f720259138cd2
Julian Ruiz <michaeljulian50@gmail.com>
2026-06-16 01:53:26 -0500

    Feat:add, variables env

--- routes/animals.js
+++ routes/animals.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../db/config');
 
-console.log('vamos a ver quien es la url', process.env.DATABASE_URL);
-
 router.get('/', async (req, res) => {
   try {
     const result = await pool.query('SELECT * FROM animals ORDER BY id');
